fbd04721a408a338b5bfe6bebff554249153e150
unknown <nguyenmai0102.meo@gmail.com>
2022-09-26 07:58:21 +0700

    noi dung

---
+++ src/chuyenDoiTIenTe/main2.java
@@ -0,0 +1,23 @@
+package chuyenDoiTIenTe;
+
+import java.util.Scanner;
+
+public class main2 {
+    public static void main(String[] args) {
+        int usd, vnd;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input usd");
+        usd = scanner.nextInt();
+        vnd = usd *23000;
+        System.out.println("so tien VND :" +vnd);
+    }
+}
+
+
+
+
+
+
+
+
+
